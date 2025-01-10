d3e7f750cfd8e9657b4650fa16b6f29097738b42
Tuan Dang <tuandang2k2@gmail.com>
2025-01-11 02:07:38 +0700

    Update Profile.js

--- src/user/Profile.js
+++ src/user/Profile.js
@@ -639,7 +639,7 @@ class Profile extends Component {
                                                 top: "50%",
                                                 left: "50%",
                                                 transform: "translate(-50%, -50%)",
-                                                width: 500,
+                                                width: 1000,
                                                 bgcolor: "background.paper",
                                                 border: "2px solid #000",
                                                 boxShadow: 24,
